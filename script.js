5531b20507e4baa1f0428157035d94a4e29d6ec8
ERICK SEEBASTIAN <ERICKSHAZ730@GMAIL.COM>
2024-10-27 12:52:25 -0600

    Subiendo archivos
    Se anexan los archivos index, style y script

---
+++ script.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('login-form');
+const UsernameInput = document.getElementById('Username');
+const passwordInput = document.getElementById('Password');
+const errorMessage = document.getElementById('error-message');
+
+//simulacion de credenciales de acceso
+const validUsername = 'Erick';
+const validPassword = '12345';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const enterdUsername = UsernameInput.value.trim();
+    const enterPassword = passwordInput.value;
+ 
+    if (enterdUsername === validUsername && enterPassword === validPassword) {
+        alert('Inicio de secion exitoso!')
+       //redirige a la pagina//
+        window.location.href = '';
+    } else {
+        alert('Falla de inicio de secion')
+    }
+
+})
